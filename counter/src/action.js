08d715af1dcd05cab8025695496eda9545bf3c93
Slavus54 <54140819+Slavus54@users.noreply.github.com>
2020-02-22 16:45:35 +0700

    Add files via upload

--- counter/src/action.js
+++ counter/src/action.js
@@ -1,12 +1,31 @@
-import {ADD, SUB} from './actionTypes';
+import {ADD, SUB, DROP} from './actionTypes';
 
-export function add () {
+export function add (number) {
 	return {
-		type: ADD
+		type: ADD,
+		payload: number
 	}
 }
 export function sub () {
 	return {
 		type: SUB
+	}
+}
+export function async (number) {
+	 return (dispatch) => {
+		 setTimeout(() => {
+	  		dispatch(add(number))
+	  	}, 1500)
+	 console.log('Async here');
+	 }
+	// return {
+	// 	type: ASS,
+	// 	payload: number
+	// } 
+}
+export function drop () {
+	return {
+		type: DROP,
+		payload: 0
 	}
 }
--- counter/src/actionTypes.js
+++ counter/src/actionTypes.js
@@ -1,2 +1,4 @@
 export const ADD = 'ADD'
 export const SUB = 'SUB'
+export const DROP = 'DROP'
+export const ASS = 'ASS'
