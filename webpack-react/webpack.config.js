3e115ca2b9fa0b6ef1582258a047b96a75acd676
Slavus54 <54140819+Slavus54@users.noreply.github.com>
2020-02-22 16:56:49 +0700

    Add files via upload

---
+++ webpack-react/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'index_bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {presets: ['env', 'react']}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		})
+	],
+	resolve: {
+    	modules: ['node_modules'],
+    	extensions: [".js", ".json", ".jsx", ".css"]
+  	},
+  	devServer: {
+    	clientLogLevel: 'silent'
+  	}
+}
